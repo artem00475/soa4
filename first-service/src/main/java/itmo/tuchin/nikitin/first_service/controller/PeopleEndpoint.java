79427f83fdb98bdc894ff35375d47f3cf36cb3b5
Egor Nikitin <egornikitin1401@gmail.com>
2024-12-27 20:00:34 +0300

    second

--- first-service/src/main/java/itmo/tuchin/nikitin/first_service/controller/PeopleEndpoint.java
+++ first-service/src/main/java/itmo/tuchin/nikitin/first_service/controller/PeopleEndpoint.java
@@ -2,20 +2,24 @@ package itmo.tuchin.nikitin.first_service.controller;
 
 import itmo.tuchin.nikitin.first_service.entity.Color;
 import itmo.tuchin.nikitin.first_service.entity.Country;
+import itmo.tuchin.nikitin.first_service.service.PersonService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-import se.ifmo.ru.firstservice.person.ColorEnum;
-import se.ifmo.ru.firstservice.person.CountryEnum;
-import se.ifmo.ru.firstservice.person.GetColorResponse;
-import se.ifmo.ru.firstservice.person.GetCountryResponse;
+import se.ifmo.ru.firstservice.person.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Endpoint
-public class EnumEndpoint {
+@RequiredArgsConstructor
+public class PeopleEndpoint {
     private static final String NAMESPACE_URI = "http://se/ifmo/ru/firstservice/person";
+    private final PersonService personService;
+    private final ModelMapper modelMapper;
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getColorRequest")
     @ResponsePayload
@@ -33,6 +37,24 @@ public class EnumEndpoint {
         GetCountryResponse response = new GetCountryResponse();
         List<CountryEnum> data = response.getData();
         data.addAll(Arrays.stream(Country.values()).map(value -> CountryEnum.fromValue(value.name())).toList());
+
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getPersonRequest")
+    @ResponsePayload
+    public GetPersonResponse getPerson(@RequestPayload GetPersonRequest request) {
+        GetPersonResponse response = new GetPersonResponse();
+        response.setPerson(modelMapper.map(personService.get(request.getId()), PersonResponse.class));
+
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "addPersonRequest")
+    @ResponsePayload
+    public AddPersonResponse addPerson(@RequestPayload AddPersonRequest request) {
+        AddPersonResponse response = new AddPersonResponse();
+        response.setPerson(modelMapper.map(personService.add(modelMapper.map(request.getPerson(), itmo.tuchin.nikitin.first_service.dto.PersonDTO.class)), PersonResponse.class));
 
         return response;
     }
